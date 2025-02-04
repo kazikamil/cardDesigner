7246d0625204de242000802f063c6472726f259c
kazi <kazi.kamil13@gmail.com>
2025-02-04 21:23:22 +0100

    m

--- Nouveau dossier/src/middleware.ts
+++ Nouveau dossier/src/middleware.ts
@@ -9,19 +9,21 @@ export async function middleware(req: NextRequest) {
     const token = req.nextUrl.searchParams.get("token");
     if (!token) {
         console.log("❌ Aucun token fourni !");
-        return NextResponse.redirect("https://www.eurequat-algerie.com/mon-compte/");
+        //return NextResponse.redirect("https://www.eurequat-algerie.com/mon-compte/");
+        return;
     }
 
     try {
+        console.log(token)
         const { payload } = await jwtVerify(token, secretKey);
         console.log("✅ Token validé :", payload);
         return NextResponse.next();
     } catch (error:any) {
         console.log("❌ Token invalide :", error.message);
-        return NextResponse.redirect("https://www.eurequat-algerie.com/mon-compte/");
+        //return NextResponse.redirect("https://www.eurequat-algerie.com/mon-compte/");
     }
 }
 
 export const config = {
-    matcher: "/",
+    matcher: ["/","/impression_etiquette_en_ligne"],
 };
